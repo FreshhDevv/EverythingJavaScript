3605537b408b918112c44a51c64d93d7945e57cd
FreshDev <billyhans90@gmail.com>
2022-10-22 22:26:12 +0100

    function declaration and expression

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,22 +1,18 @@
-// if statements
-// const age = 25;
-
-// if(age > 20) {
-//     console.log('You are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if (ninjas.length > 3) {
-//     console.log("that's a lot of ninjas");
-// }
-
-const password = 'pass';
-
-if (password.length >= 12) {
-    console.log('that password is mighty strong');
-} else if (password.length >= 8) {
-    console.log('that password is long enough');
-} else {
-    console.log('password is not long enough');
+// function declaration 
+function greet(){
+    console.log('hello there');
 }
+
+// function expression
+const speak = function(){
+    console.log('good day!');
+}
+
+greet();
+greet();
+greet();
+
+speak();
+speak();
+speak();
+speak();
