70e1c2ae00238aba8fd884920b7bfdf0621a938a
FreshDev <billyhans90@gmail.com>
2022-10-22 22:03:11 +0100

    If statements

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,8 +1,22 @@
-// do while loops
+// if statements
+// const age = 25;
 
-let i = 3;
+// if(age > 20) {
+//     console.log('You are over 20 years old');
+// }
 
-do {
-    console.log('val of i is: ', i);
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if (ninjas.length > 3) {
+//     console.log("that's a lot of ninjas");
+// }
+
+const password = 'pass';
+
+if (password.length >= 12) {
+    console.log('that password is mighty strong');
+} else if (password.length >= 8) {
+    console.log('that password is long enough');
+} else {
+    console.log('password is not long enough');
 }
-while (i < 5)
