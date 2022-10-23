5c991e0aa61c0f3a1e3a33e54e01e4d039763530
FreshDev <billyhans90@gmail.com>
2022-10-23 22:58:24 +0100

    Arrow functions

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,18 +1,26 @@
-// function declaration 
-function greet(){
-    console.log('hello there');
+// regular function 
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// arrow function
+// const calcArea = radius =>  3.14 * radius**2;
+
+
+
+// const area = calcArea(5);
+// console.log('area is:', area);
+
+// practice arrow functions
+
+const greet = () => 'hello world';
+
+const bill = (products, tax) => {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
 }
 
-// function expression
-const speak = function(){
-    console.log('good day!');
-}
-
-greet();
-greet();
-greet();
-
-speak();
-speak();
-speak();
-speak();
+console.log(bill([10, 15, 30], 0.2));
