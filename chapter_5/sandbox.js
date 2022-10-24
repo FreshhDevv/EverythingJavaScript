b0f36256055fac1c6e1e8015e2ca1e1f1796159f
FreshDev <billyhans90@gmail.com>
2022-10-24 15:13:38 +0100

    The math object

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,35 +1,18 @@
-// object literals
+// Math object
 
-// const blogs = [
-//     {title: 'why mac and cheese rules', likes: 30},
-//     {title: '10 things to make with marmite', likes: 23}
-// ];
+console.log(Math)
+console.log(Math.PI);
+console.log(Math.E);
 
-// console.log(blogs);
+const area = 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
-let user = {
-    name: 'crystal', 
-    age: 30,
-    email: 'crystal@gmail.com',
-    location: 'cameroon',
-    blogs: [
-        {title: 'why mac and cheese rules', likes: 30},
-        {title: '10 things to make with marmite', likes: 23},
-    ],
-    login() {
-        console.log('user logged in');
-    },
-    logout() {
-        console.log('the user logged out');
-    }, 
-    logBlogs() {
-        // console.log(this.blogs);
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog =>  {
-            console.log(blog.title, blog.likes);
-        });
-    }
-};
+// random numbers
 
-user.logBlogs();
-console.log(this);
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
