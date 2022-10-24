13798a4477401f4eacacd7fa4a900d5825bdbc0c
FreshDev <billyhans90@gmail.com>
2022-10-24 14:33:41 +0100

    Adding methods to objects

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,16 +6,16 @@ let user = {
     email: 'crystal@gmail.com',
     location: 'cameroon',
     blog: ['why mac and cheese rules', '10 things to make with marmite'],
+    login: function() {
+        console.log('user logged in');
+    },
+    logout: function() {
+        console.log('the user logged out');
+    }, 
+    logBlogs: function() {
+        
+    }
 };
 
-console.log(user);
-console.log(user.name);
-
-// user.age = 35;
-console.log(user.age);
-console.log(user['email']);
-
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
+user.login();
+user.logout();
