cbf579dd2e67b740113af48ee93f6ac84a7a05a4
FreshDev <billyhans90@gmail.com>
2022-10-27 13:17:59 +0100

    Other ways to query the DOM

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,10 +1,15 @@
-// const para = document.querySelector("body > h1");
-
-// console.log(para);
-
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
+// get an element by ID
+const title = document.getElementById('page-title');
+console.log(title);
 
 
-
+// get elements by their class name
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+
+
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[0]);
