3230b4da915ff08c6a047707e69cf75cf10b1d48
FreshDev <billyhans90@gmail.com>
2022-10-28 22:44:59 +0100

    Regex basics

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -5,4 +5,19 @@ form.addEventListener("submit", event => {
     event.preventDefault();
     // console.log(username.value);
     console.log(form.username.value);
-})
+});
+
+// testing RegEx
+const username = 'shasun';
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+
+// if(result) {
+//     console.log('regex test passed :) ');
+// } else {
+//     console.log('regex test failed :( ');
+// }
+
+let result = username.search(pattern);
+console.log(result);
