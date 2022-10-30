9f30365454861e8c8c71a9abdea1e01c57f3b3db
FreshDev <billyhans90@gmail.com>
2022-10-30 12:15:09 +0100

    The find method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,38 +1,5 @@
-// reduce method
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+// find method
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-// const result = scores.reduce((acc, curr) => {
-//     if(curr > 50) {
-//         acc++;
-//     }
-//     return acc;
-// }, 0);
-
-// console.log(result)
-
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-    {player: 'mario', score: 90},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 30},
-    {player: 'crystal', score: 60},
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 80},
-    {player: 'crystal', score: 60},
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-    if(curr.player === 'mario') {
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-console.log(marioTotal);
+const firstHighScore = scores.find(score => score > 50);
+console.log(firstHighScore);
