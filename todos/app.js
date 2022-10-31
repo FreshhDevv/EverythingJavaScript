4627c58d9ac1c8f56f87a7d89d3e7fdcb42cca5b
FreshDev <billyhans90@gmail.com>
2022-10-31 09:43:34 +0100

    Add todos

---
+++ todos/app.js
@@ -0,0 +1,23 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="fa-solid fa-trash-can delete"></i>                
+        </li>
+    `;
+    list.innerHTML += html;
+}
+
+addForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
