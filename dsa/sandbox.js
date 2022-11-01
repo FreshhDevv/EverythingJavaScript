d8ed3653b1a0fe49b1339b39e33d070240f27697
FreshDev <billyhans90@gmail.com>
2022-11-01 13:27:10 +0100

    primality test improved

--- dsa/sandbox.js
+++ dsa/sandbox.js
@@ -22,7 +22,7 @@
 // PRIMALITY TEST
 
 const isPrime = (number) => {
-    for(let i = 2; i < number; i++) {
+    for(let i = 2; i < Math.sqrt(number); i++) {
         if(number % i === 0) {
             return false;
         }
