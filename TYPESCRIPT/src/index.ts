5f570ed81b13702ad74e9fb75685e2ca81be0fd7
FreshDev <billyhans90@gmail.com>
2022-11-03 22:14:19 +0100

    The any type

--- TYPESCRIPT/src/index.ts
+++ TYPESCRIPT/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World');
+let sales = 123_456_789;
+let course = 'Typescript course';
+let is_published = true;
+let level;
+
+function render(document) {
+    console.log(document);
+}
