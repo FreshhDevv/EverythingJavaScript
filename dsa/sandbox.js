013851fc4abe5ef89bbb0662cf36a6baa0756e8b
FreshDev <billyhans90@gmail.com>
2022-11-03 21:09:30 +0100

    bitwise notation

--- dsa/sandbox.js
+++ dsa/sandbox.js
@@ -57,14 +57,7 @@ const isPowerOfTwo = (number) => {
     if(number < 1) {
         return false;
     }
-    let dividedNumber = number;
-    while (dividedNumber !== 1) {
-        if(dividedNumber % 2 !== 0){
-            return false;
-        }
-        dividedNumber = dividedNumber / 2;
-    }
-    return true;
+    return (number & (number - 1)) === 0;
 }
 
 console.log(isPowerOfTwo(8));
