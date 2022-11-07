6838aa5c676f1e7d98ec1166ad06aa2f37523fd8
FreshDev <billyhans90@gmail.com>
2022-11-07 12:07:37 +0100

    Fixed minor bug

--- REACT/counter-app/src/components/counter.jsx
+++ REACT/counter-app/src/components/counter.jsx
@@ -5,19 +5,17 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = product => {
-    console.log(product)
-    this.setState({count: this.state.count + 1})
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
-
-
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={ () => this.handleIncrement(product) }
+          onClick={(product) => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
