970ecf4c64c9c71400b1ebfdec9c29000b1555a5
FreshDev <billyhans90@gmail.com>
2022-11-10 21:23:03 +0100

    Project complete

--- REACT/counter-app/src/App.js
+++ REACT/counter-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   state = {
     counters: [
-      { id: 1, value: 7 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
@@ -19,6 +19,15 @@ class App extends Component {
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    // console.log(this.state.counters[0]);
+    this.setState({ counters });
+  }
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     // console.log(this.state.counters[0]);
     this.setState({ counters });
   }
@@ -41,7 +50,7 @@ class App extends Component {
       <React.Fragment>
         <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
         <main className='container'>
-          <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete} />
+          <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete} />
         </main>
       </React.Fragment>
     );
