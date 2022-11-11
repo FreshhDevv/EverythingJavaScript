0c44e25baa4a1b7ef4f82ad495bd35ad7fff964f
FreshDev <billyhans90@gmail.com>
2022-11-11 14:11:19 +0100

    Use ref hook with typescript

--- REACT/taskify/src/App.tsx
+++ REACT/taskify/src/App.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import InputField from './components/inputField';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import InputField from "./components/inputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+
+const [todo, setTodo] = useState<string>('');
+
+// This is how you create an array of a type or an interface.
+const [todos, setTodos] = useState<Todo[]>([]);
+
+const handleAdd = (event: React.FormEvent) => {
+  event.preventDefault();
+
+  if(todo){
+    setTodos([...todos,{id:Date.now(), todo, isDone: false}]);
+    setTodo("");
+  }
+
+}
+
+console.log(todos);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {/* <TodoList /> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- REACT/taskify/src/components/inputField.tsx
+++ REACT/taskify/src/components/inputField.tsx
@@ -1,13 +1,37 @@
-import * as React from 'react';
-import './styles.css'
+import * as React from "react";
+import { useRef } from "react";
+import "./styles.css";
 
-const InputField = () => {
-    return (
-        <form className='input'>
-<input type='input' placeholder ='Enter a task' className='input_box'></input>
-<button className='input_submit' type='submit'>Go</button>
-        </form>
-    );
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (event: React.FormEvent) => void;
 }
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form className="input" onSubmit={(event) => {
+      handleAdd(event)
+      inputRef.current?.blur()
+      }}>
+      <input
+      ref={inputRef}
+        type="input"
+        value={todo}
+        onChange ={
+            (e) => setTodo(e.target.value)
+        }
+        placeholder="Enter a task"
+        className="input_box"
+      ></input>
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
 
 export default InputField;
