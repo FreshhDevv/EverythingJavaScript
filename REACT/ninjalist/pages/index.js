ff71bc35fefba3b390a82becd37d96b098735359
FreshDev <billyhans90@gmail.com>
2022-11-12 16:10:59 +0100

    Learning Next js

---
+++ REACT/ninjalist/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Lorem, ipsum</p>
+    </div>
+  )
+}
