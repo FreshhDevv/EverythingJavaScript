690c158b2f3cf1cc76e836240b203fcf0954bcc7
FreshDev <billyhans90@gmail.com>
2022-11-14 21:31:22 +0100

    Fetching data

--- REACT/ninjalist/pages/ninjas/index.js
+++ REACT/ninjalist/pages/ninjas/index.js
@@ -1,18 +1,36 @@
 import Head from "next/head";
+import Link from "next/link";
+import styles from '../../styles/Ninjas.module.css';
 
-const Ninjas= () => {
-    return ( 
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
+
+const Ninjas = ({ ninjas }) => {
+    return (
         <>
-        <Head>
+            <Head>
                 <title>Ninja List | Ninjas</title>
                 <meta name='keywords' content='ninjas' />
             </Head>
             <div>
                 <h1>All Ninjas</h1>
+                {ninjas.map(ninja => (
+                    <div key={ninja.id}>
+                        <Link className={styles.single}>
+                            <h3>{ninja.name}</h3>
+                        </Link>
+                    </div>
+                ))}
             </div>
         </>
-        
-     );
+
+    );
 }
- 
+
 export default Ninjas;
