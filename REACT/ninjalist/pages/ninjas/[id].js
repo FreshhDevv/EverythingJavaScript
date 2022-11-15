279ecb53222194bb7824fef594dd4765dd27b1b7
FreshDev <billyhans90@gmail.com>
2022-11-15 10:30:45 +0100

    Dynamic routes part 2

--- REACT/ninjalist/pages/ninjas/[id].js
+++ REACT/ninjalist/pages/ninjas/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPaths = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    // Returns an array of objects.
+
+    const paths = data.map(ninja => {
+        params: {id:ninja.id.toString()}
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
 const Details = () => {
     return ( 
         <div>
