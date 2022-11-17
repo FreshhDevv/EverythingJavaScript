3933c6fff833fbea46b5afaf11d92d8da5bf345c
FreshDev <billyhans90@gmail.com>
2022-11-17 11:25:52 +0100

    Updating typescript

---
+++ TYPESCRIPT/LearningTs/add.js
@@ -0,0 +1,4 @@
+//Tuple data type
+//We can define an array of tuples
+var employee;
+employee = [[1, 'Joe'], [2, 'Ella'], [3, 'Queen']];
---
+++ TYPESCRIPT/LearningTs/add.ts
@@ -0,0 +1,9 @@
+// //Tuple data type
+// //We can define an array of tuples
+// var employee: [number, string][];
+// employee = [[1, 'Joe'], [2, 'Ella'], [3, 'Queen']];
+
+//Type inference
+var arr = [0, 1, 'work'];
+arr.push('busy');
+arr.push(true);
