1f11233a107dc6f4791ffc1d77d589356edc2453
FreshDev <billyhans90@gmail.com>
2022-11-19 16:30:24 +0100

    Middleware

--- REDUX/index.js
+++ REDUX/index.js
@@ -5,6 +5,9 @@ const redux = require('redux');
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require('redux-logger');
+const logger = reduxLogger.createLogger();
 
 // The Action Type
 const CAKE_ORDERED = 'CAKE_ORDERED';
@@ -100,9 +103,9 @@ const rootReducer = combineReducers({
     iceCream: iceCreamReducer,
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial State', store.getState());
-const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
