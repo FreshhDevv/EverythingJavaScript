f23e1732e2bac634a1d09c20432b6c9447b724e9
FreshDev <billyhans90@gmail.com>
2022-11-20 13:50:23 +0100

    Logger middleware

--- REDUX TOOLKIT/index.js
+++ REDUX TOOLKIT/index.js
@@ -4,7 +4,7 @@ const icecreamActions = require('./app/features/icecream/icecreamSlice').icecrea
 
 console.log('Initial state ', store.getState());
 const unsubscribe = store.subscribe(() => {
-    console.log('Updated state ', store.getState());
+    // console.log('Updated state ', store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
