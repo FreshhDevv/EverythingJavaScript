285cca3167ebf77d9d04fbd7d09a4be7f3158bc5
FreshDev <billyhans90@gmail.com>
2022-11-21 23:10:47 +0100

    Strings

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,27 @@
+// Strings 
+
+console.log('Hello, world');
+
+let email = 'billyhans@gmail.com';
+console.log(email);
+
+// String Concatenation
+let firstName = 'Billy';
+let lastName = 'Hans';
+
+let fullName = firstName + ' ' +lastName;
+console.log(fullName);
+
+// Getting Characters
+console.log(fullName[0]);
+
+// String Length
+console.log(fullName.length);
+
+// String Methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
