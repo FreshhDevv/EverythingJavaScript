c9ccd89d04b3f724b57f8e6517baaa0014f6be16
FreshDev <billyhans90@gmail.com>
2022-11-22 11:07:08 +0100

    Fetching data

--- REDUX TOOLKIT/Redux Toolkit With React/react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ REDUX TOOLKIT/Redux Toolkit With React/react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -10,8 +10,9 @@ export const IcecreamView = () => {
     <div>
       <h2>Number of ice creams - {numOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order ice cream</button>
-      <input type={number} value = {value} onChange={e => setValue(parseInt(e.target.value))}
+      <input type='number' value = {value} onChange={e => setValue(parseInt(e.target.value))}/>
       <button onClick={() => dispatch(restocked(value))}>Restock ice creams</button>
     </div>
   );
 };
+ 
